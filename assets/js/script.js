8a0921b675a793cd063ea0d0501494bf89069a2b
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-07-27 21:42:44 -0700

    adding javascript logic for clicks and questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Variables to keep track of the current state of quiz
 var time = questions.length * 15;
 var timerId;
+var currentQuestionIndex = 0;
 
 // DOM variables
 var timerEl = document.getElementById("time");
@@ -13,12 +14,66 @@ function startQuiz() {
     startScreenEl.setAttribute("class", "hide");
 
     //start timer
-    timerID = setInterval(clockTick, 1000);
+    timerID = setInterval(countDown, 1000);
     
 
     // show start time
     timerEl.textContent = time;
 
     getQuestions();
+}
+
+function getQuestion(); {
+// get question objects from array
+var currentQuestion = questions [currentQuestionIndex];
+
+
+var titleEl = document.getElementById("question-title");
+titleEl.textContent = currentQuestion.title;
+
+// clear old choices
+choicesEl.innterHTML = "";
+
+// loop choices
+currentQuestion.choices.forEach(function(choice, i) {
+    var choiceNoce = document.createElement("button");
+    choiceNode.setAttribute("class", "choice");
+    choiceNode.setAttribute("value", choice);
+
+    choiceNode.textContent = i + 1 + ". " + choice;
+
+    choiceNode.onclick = questionClick;
+
+    choiceEl.appendChild(choiceNode);
+});
+
+// check answers to questions upon click
+function questionClick() {
+    if (this.value !== questions[currentQuestionIndex].answer) {
+        time-= 15;
+    }
+
+    if (time < 0) {
+        time = 0;
+    }
+
+// update time
+    timeEl.textContent = time;
+
+    feedbackEl.textContent = "Incorrect"; 
+
+    } else {
+
+        feedbackEl.textContent = "Correct"
+    }
+
+}
+
+function countDown() {
+    time--;
+    timerEl.textContent = time;
+
+}
+
 
 }
